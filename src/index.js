bf6f0f653b8dd1d927a2a2fdb351b2572ea18649
Dan King <king.daniel.thomas@gmail.com>
2023-08-04 15:45:58 +0700

    Add asset imports css loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import './meyer-reset.css'
+import './style.css'
+const pageHeader = document.querySelector('.header')
+
+pageHeader.classList.add('hello')
+
+
 console.log('Welcome to Battle Sandwich!')
+
