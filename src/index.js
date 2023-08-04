4a4cde07571dba3028353403eaa0234f56240816
Dan King <king.daniel.thomas@gmail.com>
2023-08-04 15:51:35 +0700

    Add image and font imports to webpack

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+/* Imports */
 import './meyer-reset.css'
 import './style.css'
+
+/* Memory allocation */
 const pageHeader = document.querySelector('.header')
 
+/* Application */
 pageHeader.classList.add('hello')
 
-
 console.log('Welcome to Battle Sandwich!')
-
