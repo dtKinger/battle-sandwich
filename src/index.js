3f6611ab8677c5a288073a5c1c793a599d59a858
Dan King <king.daniel.thomas@gmail.com>
2023-08-04 16:04:01 +0700

    Import a 2nd js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 /* Imports */
 import './meyer-reset.css'
 import './style.css'
+import './secondary.js'  
+// import Icon from './icon.png';  // these don't exist
+// import Data from './data.xml';
+// import Notes from './data.csv';
 
 /* Memory allocation */
 const pageHeader = document.querySelector('.header')
